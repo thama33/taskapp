02073b17f49d721d37765635e8526089618d6fd4
takuya.hamagami <thama0727@gmail.com>
2022-04-16 22:40:38 +0900

    Lesson6 7.5 まで完成

--- taskapp/AppDelegate.swift
+++ taskapp/AppDelegate.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate,UNUserNotificationCenterDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        //ユーザーに通知の許可を求める
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert, .sound]){(granted,error) in
+            
+        }
+        center.delegate = self
+        
         return true
     }
 
@@ -29,6 +37,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+    }
+    
+    //アプリがフォアグランドの時に通知を受け取ると呼ばれるメソッド
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .list, .sound])
     }
 
 
---
+++ taskapp/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by 濱上卓也 on 2022/04/15.
+//
+
+import RealmSwift
+
+
+class Task: Object{
+    //管理用　ID プライマリーキー
+    @objc dynamic var id = 0
+    
+    //タイトル
+    @objc dynamic var title = ""
+    
+    //内容
+    @objc dynamic var contents = ""
+    
+    //日時
+    @objc dynamic var date = Date()
+    
+    //カテゴリー
+    
+//    @objc dynamic var category = ""
+    
+    // idをプライマリーキーとして設定
+    
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
